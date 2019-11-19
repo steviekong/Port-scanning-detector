bf65519af469b358ea754be57f4f51ef14e96a94
steviekong <sidharth.reji@gmail.com>
2019-11-19 14:01:04 -0500

    squished some messy bugs

--- detector.py
+++ detector.py
@@ -8,33 +8,36 @@ def main():
 	reader = PcapReader(sys.argv[1])
 	syn_dict = {}
 	syn_ack_dict = {}
-	ip_list = []
+	ip_list = set()
 	for p in reader:
 		if TCP in p and IP in p:
 			f = p['TCP'].flags
-			if f & 0x02:
-				if p[IP].src not in ip_list:
-					ip_list.append(p[IP].src)
-					print(ip_list)
+			if f == 'S':
+				ip_list.add(p[IP].src)
 				if p[IP].src in syn_dict:
 					syn_dict[p[IP].src] += 1
 				else:
 					syn_dict[p[IP].src] = 1
-			if f & 0x012:
-				if p[IP].src not in ip_list:
-					ip_list.append(p[IP].src)
-				if p[IP].src in syn_ack_dict:
-					syn_ack_dict[p[IP].src] += 1
+			if f == 'SA':
+				ip_list.add(p[IP].dst)
+				if p[IP].dst in syn_ack_dict:
+					syn_ack_dict[p[IP].dst] += 1
 				else:
-					syn_ack_dict[p[IP].src] = 1
+					syn_ack_dict[p[IP].dst] = 1
 	filtered_ip_list = []
 	for i in ip_list:
 		if i in syn_dict and i in syn_ack_dict:
-			if syn_dict[i]/syn_ack_dict[i] > 3:
+			if syn_dict[i]/syn_ack_dict[i] >= 3:
 				filtered_ip_list.append(i)
 		if i in syn_dict and i not in syn_ack_dict:
 			filtered_ip_list.append(i)
-	print(filtered_ip_list)
+	
+	if len(filtered_ip_list) == 0:
+		print("No port scanners found in the given file")
+	else:
+		print("The following IP's were found to have port scanners")
+		for i in filtered_ip_list:
+			print(i)
 
 if __name__ == '__main__':
 	main()
